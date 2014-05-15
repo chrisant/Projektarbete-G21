13b0272114a7afe3ad7f8260e0d35dba4cfe4432
Linus Staf <email@linusstaf.com>
2014-05-15 09:16:57 +0200

    Snyggade till lite

--- autoload.php
+++ autoload.php
@@ -1,28 +1,65 @@
 <?php
 
-// Definiera variabler för att enkelt kunna länka till vår applikations filer.
-// Det är bra att definiera dessa för att undvika problem ifall mappstrukturen
-// skulle ändras i framtiden.
-define('ROOT', realpath(dirname(__FILE__)) . '/');
-define('APP', realpath(dirname(__FILE__)) . '/app/');
-define('LIB', realpath(dirname(__FILE__)) . '/libraries/');
-define('PUBLIC_PATH', realpath(dirname(__FILE__)) . '/public/');
+/*
+ * ----------------------------------------
+ *  CONSTANTS
+ * ----------------------------------------
+ *
+ * En typ av variabler man kan ange värden på för att lätt använda senare.
+ * Bra för att länka till filer med mera.
+ *
+ */
 
-// Felhantering
+define('ROOT',          realpath(dirname(__FILE__)) . '/');
+define('APP',           realpath(dirname(__FILE__)) . '/app/');
+define('LIB',           realpath(dirname(__FILE__)) . '/libraries/');
+define('CONFIG',        realpath(dirname(__FILE__)) . '/config/');
+define('PUBLIC_PATH',   realpath(dirname(__FILE__)) . '/public/'); // Ändra till att detecta serverns URL....
+
+
+/*
+ * ----------------------------------------
+ *  Felhantering
+ * ----------------------------------------
+ *
+ * Alla error sidor och felmeddelanden ska fångas i olika exceptions.
+ * Helt enkelt ska applikationen köra try-catch på de flesta requester.
+ *
+ */
+
 include LIB . 'Exceptions/NotFoundException.php';
 
-// Inkludera Views
+/*
+ * ----------------------------------------
+ *  Views
+ * ----------------------------------------
+ *
+ * Views används för att bygga upp själva sidorna användarna ser.
+ * En kontroller skickar data från modeller till en vy som sedan
+ * bygger upp en sida med de variablerna.
+ *
+ */
+
 include LIB . 'View/Helpers.php';
 include LIB . 'View/View.php';
 
 
-// Ej komplett routing.
-// Kanske lite väl avancerad routing för vårt projekt, men i en riktig app hade
-// Routing nog implementerats mer åt det här hållet.
+/*
+ * ----------------------------------------
+ *  Routing
+ * ----------------------------------------
+ *
+ * EJ FULLT FUNKTIONELL
+ * Enkel routing körs inuti konstruktorn på Bootstrap för tillfället.
+ *
+ */
 
-// Inkludera Routing
 include LIB . 'Routing/Controller.php';
-//include BASE_PATH . 'libraries/Routing/Router.php';
-//include BASE_PATH . 'libraries/Routing/Route.php';
-//include BASE_PATH . 'config/routes.php';
+//include LIB . 'Routing/Router.php';
+//include LIB . 'Routing/Route.php';
+//include CONFIG . 'routes.php';
 
+/*
+ * Kanske lite väl avancerad routing för vårt projekt, men i en riktig app hade
+ * routing nog implementerats mer åt det här hållet.
+ */
--- public/index.php
+++ public/index.php
@@ -1,16 +1,41 @@
 <?php
-
 /*
- * För att förenkla utvecklingen använder vi följande rad:
+ * ----------------------------------------
+ *  Visa alla fel & varningar
+ * ----------------------------------------
+ *
+ * Under utvecklingen visar vi mer än det som vanligtvis visar om
+ * något är fel i vår kod. Även varningar som inte får applikationen
+ * att krascha kommer visas i webbläsaren.
+ *
  */
+
+
 error_reporting(E_ALL);
 
-/*
- * Ladda de filer som behövs
- */
-require_once __DIR__.'/../autoload.php';
-require __DIR__ . '/../libraries/bootstrap.php';
 
-// Instantiera vår app
-$app = new Bootstrap();
+/*
+ * ----------------------------------------
+ *  Registrera autoloadern
+ * ----------------------------------------
+ *
+ * Ladda alla de filer som behövs för att starta
+ *
+ */
+
+require __DIR__.'/../autoload.php';
+
+
+/*
+ * ----------------------------------------
+ *  Starta applikationen
+ * ----------------------------------------
+ *
+ * Här instantierar vi applikationen så vi kan använda fårt framework.
+ *
+ */
+
+$app = require_once LIB . 'bootstrap.php';
+
+
 
