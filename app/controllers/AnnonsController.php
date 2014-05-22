ef63c3509de2f89c459b078badd597b85154bb0e
djwilc <d.j.wilc@gmail.com>
2014-05-22 12:20:35 +0200

    Update AnnonsController.php

--- app/controllers/AnnonsController.php
+++ app/controllers/AnnonsController.php
@@ -27,21 +27,45 @@ class AnnonsController extends Controller {
 
     public function spara()
     {
+        $input = [
+        'description' => str_replace( $_POST['description']),
+    	'images'      => str_replace( $_POST['images']),
+    	'title'       => str_replace( $_POST['title'])
+    	];
+        
+        
         // Instansiera ett objekt av typen annons
-
+        $annons = new Annons();
         // hämta en validator med annonsens valideringsregler
-
+        $validator = new Validator(@annons -> rules);
         // validera input
-
+        $errors  = $validator->validate($input);
         // generera egen data:
-
+        $author = $_SESSION['id'];
         // Skapa taggar
             // instansiera objekt av typen "tag"
-
+        $array = explode(' ', $input['title']);
+        $array[] = explode(',', $_POST['label']);
         // Länka taggarna till annonsen
             // dvs skapa annons-tag-pivot värden
-
+        $annons->create([
+        'description' => $input['description'],
+    	'images'      => $input['images'],
+    	'title'       => $input['title'], 
+        'author'      => $input['author']
+            
+        ]);    
+        $annons-> save();
+        $id = $annons->id;
+        
+        foreach($array as $tags)
+        {
+            $tag = new Tag($tag);
+            $pivot = new Pivot($id, $tag-id)
+            
+        }
         // Meddela användaren om resultatet
+        $_SESSION['message'] = 'Din annons är nu skapad!';
 
     }
 
