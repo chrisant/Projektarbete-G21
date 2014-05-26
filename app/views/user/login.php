a251688ed1b325354ac52934b0c9bce7106cbe73
chrisant <christian.antonsen@hotmail.com>
2014-05-26 15:39:39 +0200

    Update login.php

--- app/views/user/login.php
+++ app/views/user/login.php
@@ -1,6 +1,6 @@
 <h2>Logga in</h2>
-<form action="<?= PUBLIC_PATH ?>/login/post" method="post">
+<form action="<?= PUBLIC_PATH ?>/login/post" method="post" name="login-form">
     <input type="email" name="email" placeholder="E-post">
     <input type="password" name="password" placeholder="Lösenord">
-    <input type="submit" value="Logga in">
+    <input type="submit" value="Logga in" id="submitBtn">
 </form>
